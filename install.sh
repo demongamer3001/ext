53444af8f6d2a775affe3179996194482cfc5e6f
demongamer3001 <demongamer3001@gmail.com>
2022-04-09 01:16:41 +0200

    Added install script.

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo This script is going to move the 'ext' program to the /usr/bin folder.
+echo Press CTRL+C to exit or ENTER to continue.
+read
+echo Moving file...
+sudo mv ext /usr/bin
